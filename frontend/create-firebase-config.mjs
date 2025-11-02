8682980b9dea79272e0e5245c387e16f66488424
Govind Choudhari <choudharygovind553@gmail.com>
2025-11-03 02:49:30 +0530

    added firebase config file

--- frontend/create-firebase-config.mjs
+++ frontend/create-firebase-config.mjs
@@ -1,19 +1,19 @@
 import fs from 'fs';
 
-const configFilePath = './firebaseConfig.js';
+const configFilePath = './frontend/firebaseConfig.js';
 
 const firebaseConfigContent = `
 export const firebaseConfig = {
-    apiKey: "${process.env.PUBLIC_FIREBASE_API_KEY}",
-    authDomain: "${process.env.PUBLIC_FIREBASE_AUTH_DOMAIN}",
-    projectId: "${process.env.PUBLIC_FIREBASE_PROJECT_ID}",
-    storageBucket: "${process.env.PUBLIC_FIREBASE_STORAGE_BUCKET}",
-    messagingSenderId: "${process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID}",
-    appId: "${process.env.PUBLIC_FIREBASE_APP_ID}",
-    measurementId: "${process.env.PUBLIC_FIREBASE_MEASUREMENT_ID}"
+  apiKey: "${process.env.PUBLIC_FIREBASE_API_KEY}",
+  authDomain: "${process.env.PUBLIC_FIREBASE_AUTH_DOMAIN}",
+  projectId: "${process.env.PUBLIC_FIREBASE_PROJECT_ID}",
+  storageBucket: "${process.env.PUBLIC_FIREBASE_STORAGE_BUCKET}",
+  messagingSenderId: "${process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID}",
+  appId: "${process.env.PUBLIC_FIREBASE_APP_ID}",
+  measurementId: "${process.env.PUBLIC_FIREBASE_MEASUREMENT_ID}"
 };
 `;
 
 fs.writeFileSync(configFilePath, firebaseConfigContent);
 
-console.log('Successfully created firebaseConfig.js for deployment!');
+console.log('✅ Successfully created firebaseConfig.js for deployment inside /frontend!');
